0a51b73c33ae478191fbdfe6d3c97b1a24177957
saikun1 <saikun723@gmail.com>
2023-04-15 10:37:12 +0700

    Tugas ke-16 php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
+<html>
+<head>
+<title>Membuat Biodata Sederhana</title>
+</head>
+<body style="background-color:silver">
+<table border="1" style="width: 700px;">
+<tr><td align="center" colspan="3" bgcolor="red"><font color="blue" size="20px">
+<b>BIODATA</b>
+</font></td></tr>
+<tr>
+<td rowspan="5" width="100px">
+<img src="image/foto1.jpeg" width="150px" height="200px" border="2">
+</td>
+<td bgcolor="#0000FF">NAMA </td>
+<td> Saikun </td>
+</tr>
+<tr>
+<td bgcolor="#0000FF"> UMUR </td>
+<td> 23 Tahun </td>
+</tr>
+<tr>
+<td bgcolor="#0000FF"> ALAMAT </td>
+<td> Jl. Pasundan. No. 108 Kota Bandung </td>
+</tr>
+<tr>
+<td bgcolor="#0000FF"> HOBI </td>
+<td> Olahraga </td>
+</tr>
+<tr>
+<td align="center" colspan="2" bgcolor="white"> Kunjungi Website :<a href="https://www.arkatama.com">WWW.ARKATMA.COM</a> </td>
+</tr>
+</table>
+</body>
+</html>
